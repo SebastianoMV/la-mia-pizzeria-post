0681a42f00b85ffe7d03235722303889659377dd
SebastianoMV <sebastianovisco@gmail.com>
2022-10-05 13:58:11 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_post.Models
+{
+    public class Pizza
+    {
+        public Pizza(string nome, string descrizione, string image, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Image = image;
+            Prezzo = prezzo;
+
+        }
+
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Image { get; set; }
+        public double Prezzo { get; set; }
+    }
+}
